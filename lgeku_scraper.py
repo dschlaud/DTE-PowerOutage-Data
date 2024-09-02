9665bb1c8cd2de646f1b645c589d62b367ca04d7
RestoredFawn12 <99234073+dschlaud@users.noreply.github.com>
2024-09-02 11:54:55 -0400

    Update lgeku_scraper.py

--- lgeku_scraper.py
+++ lgeku_scraper.py
@@ -4,7 +4,7 @@ from kubra_scraper import KubraScraper
 class LGEKUScraper(KubraScraper):
     owner = "dschlaud"
     repo = "DTE-PowerOutage-Data"
-    filepath = "lgeku/outages.json"
+    filepath = "data/DTE-outages.json"
 
     instance_id = "4fbb3ad3-e01d-4d71-9575-d453769c1171"
     view_id = "8ed2824a-bd92-474e-a7c4-848b812b7f9b"
